bf17569dba54b906728fdb6b6d50812011a4bc46
Yehuda Katz <wycats@gmail.com>
2013-05-27 12:06:57 +0100

    Update cocoapods stuff

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaAsyncSocket
+#define COCOAPODS_POD_AVAILABLE_CocoaAsyncSocket
+#define COCOAPODS_VERSION_MAJOR_CocoaAsyncSocket 0
+#define COCOAPODS_VERSION_MINOR_CocoaAsyncSocket 0
+#define COCOAPODS_VERSION_PATCH_CocoaAsyncSocket 1
+
+// LibYAML
+#define COCOAPODS_POD_AVAILABLE_LibYAML
+#define COCOAPODS_VERSION_MAJOR_LibYAML 0
+#define COCOAPODS_VERSION_MINOR_LibYAML 1
+#define COCOAPODS_VERSION_PATCH_LibYAML 4
+
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 0
+#define COCOAPODS_VERSION_PATCH_Mantle 0
+
+// YAML-Framework
+#define COCOAPODS_POD_AVAILABLE_YAML_Framework
+#define COCOAPODS_VERSION_MAJOR_YAML_Framework 0
+#define COCOAPODS_VERSION_MINOR_YAML_Framework 0
+#define COCOAPODS_VERSION_PATCH_YAML_Framework 1
+
+// libextobjc/EXTKeyPathCoding
+#define COCOAPODS_POD_AVAILABLE_libextobjc_EXTKeyPathCoding
+#define COCOAPODS_VERSION_MAJOR_libextobjc_EXTKeyPathCoding 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_EXTKeyPathCoding 2
+#define COCOAPODS_VERSION_PATCH_libextobjc_EXTKeyPathCoding 5
+
+// libextobjc/EXTScope
+#define COCOAPODS_POD_AVAILABLE_libextobjc_EXTScope
+#define COCOAPODS_VERSION_MAJOR_libextobjc_EXTScope 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_EXTScope 2
+#define COCOAPODS_VERSION_PATCH_libextobjc_EXTScope 5
+
+// libextobjc/RuntimeExtensions
+#define COCOAPODS_POD_AVAILABLE_libextobjc_RuntimeExtensions
+#define COCOAPODS_VERSION_MAJOR_libextobjc_RuntimeExtensions 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_RuntimeExtensions 2
+#define COCOAPODS_VERSION_PATCH_libextobjc_RuntimeExtensions 5
+
