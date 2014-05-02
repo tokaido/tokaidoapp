db463f2c19d7942c0ad0906b4a1b57cf179984e4
Andrés N. Robalino <andres@androbtech.com>
2014-05-01 22:53:43 -0500

    separate utilities from App Delegate

---
+++ Tokaido/TKDUtilities.h
@@ -0,0 +1,13 @@
+//
+//  TKDUtilities.h
+//  Tokaido
+//
+//  Created by Andrés N. Robalino on 01/05/14.
+//  Copyright (c) 2014 Tilde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKDUtilities : NSObject
+
+@end
---
+++ Tokaido/TKDUtilities.m
@@ -0,0 +1,13 @@
+//
+//  TKDUtilities.m
+//  Tokaido
+//
+//  Created by Andrés N. Robalino on 01/05/14.
+//  Copyright (c) 2014 Tilde. All rights reserved.
+//
+
+#import "TKDUtilities.h"
+
+@implementation TKDUtilities
+
+@end
