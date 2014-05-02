74a5a2fb0aa8a0c42c8c64e62dc67ff2de473fd1
Andrés N. Robalino <andres@androbtech.com>
2014-05-01 22:31:30 -0500

    Separate start tokaido task

---
+++ Tokaido/TKDStartTokaido.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import "TKDControlTask.h"
+#import "TKDAppSupportEnsure.h"
+
+@interface TKDStartTokaido : NSObject <TKDControlTask>
+
+@property (nonatomic, assign) id <TKDAppSupportEnsure> view;
+
+-(TKDStartTokaido *) initWithView:(id <TKDAppSupportEnsure>)view;
+
+-(Class) configuration;
+-(Class) fileManager;
+
+-(void) execute;
+
+@end
