56f1cbc0bf161fd87bcdb8de8c6c2bba43224687
Andrés N. Robalino <andres@androbtech.com>
2015-04-21 17:45:51 -0500

    2.1.5 latest, prepackaged.

--- Scripts/package.sh
+++ Scripts/package.sh
@@ -44,12 +44,12 @@ cd $TKD_TMP_PATH
 zip -r $TKD_RUBY.zip $TKD_RUBY
 cd $root
 
-rm -Rf src
-rm -Rf dist
+#rm -Rf src
+#rm -Rf dist
 
 cp $TKD_TMP_PATH/$TKD_RUBY.zip Tokaido/Rubies/$TKD_RUBY.zip
 cp $TKD_TMP_PATH/tokaido-gems.zip Tokaido/tokaido-gems.zip
 cp $TKD_TMP_PATH/tokaido-bin.zip Tokaido/tokaido-bin.zip
 cp $TKD_TMP_PATH/tokaido-bootstrap.zip Tokaido/tokaido-bootstrap.zip
 
-rm -Rf $TKD_TMP_PATH
+#rm -Rf $TKD_TMP_PATH
