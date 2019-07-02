b96131de8c1ffdb9e1e140ea472e656ed041c3c9
Andrés N. Robalino <andres@androbtech.com>
2019-07-02 16:43:35 -0500

    Ruby changed the way gem installation options work (documentation generation)

--- Scripts/gems.sh
+++ Scripts/gems.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp Tokaido/Gemfile $TKD_TMP_PATH/Gemfile
+cp Tokaido/Gemfile.$RAILS_GEMFILE $TKD_TMP_PATH/Gemfile
 cd $TKD_TMP_PATH
 
 echo "Removing existing GEM_HOME to rebuild it"
@@ -9,13 +9,11 @@ rm -rf gem_home
 echo "Building new GEM_HOME"
 mkdir -p $TKD_TMP_PATH/gem_home/ruby/$TKD_RUBY_NAMESPACE
 
-
-command -v pkg-config >/dev/null 2>&1 && bundle config --local build.nokogiri --with-opt-dir=$(pkg-config iconv --variable=prefix)
-command -v pkg-config >/dev/null 2>&1 && bundle config --local build.sqlite3 --with-opt-dir=$(pkg-config sqlite3 --variable=prefix)
+#command -v pkg-config >/dev/null 2>&1 && bundle config --local build.nokogiri --with-opt-dir=$(pkg-config iconv --variable=prefix)
+#command -v pkg-config >/dev/null 2>&1 && bundle config --local build.sqlite3 --with-opt-dir=$(pkg-config sqlite3 --variable=prefix)
 
 bundle --path gem_home --gemfile Gemfile
-
-gem install bundler -E --no-ri --no-rdoc -i gem_home/ruby/$TKD_RUBY_NAMESPACE
+gem install bundler -E $NO_DOC_OPTION -i gem_home/ruby/$TKD_RUBY_NAMESPACE
 
 rm -f tokaido-gems.zip
 rm -Rf Gems
@@ -25,10 +23,6 @@ cd Gems
 cp -R ../gem_home/ruby/$TKD_RUBY_NAMESPACE .
 cd ..
 mkdir -p Gems/supps
-#FIXME
-cp $TKD_EXTENSIONS_PATH/railties-4.2.1_app_base.rb Gems/$TKD_RUBY_NAMESPACE/gems/railties-4.2.1/lib/rails/generators/app_base.rb
-
-cp -R $TKD_SUPPLEMENTS_PATH/iconv Gems/supps/iconv
 cp -R $TKD_SUPPLEMENTS_PATH/bin_files Gems/bin_files
 
 zip -r tokaido-gems.zip Gems
