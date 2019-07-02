d19da609ccac32f107dd241de8931a40c732b438
Andrés N. Robalino <andres@androbtech.com>
2019-07-02 16:33:23 -0500

    Meant for local development.

--- Tokaido/uninstall.sh
+++ Tokaido/uninstall.sh
@@ -8,8 +8,5 @@ rm -rf ~/.tokaido
 rm -rf ~/Library/Application\ Support/Tokaido
 rm -rf /etc/resolvers/tokaido
 rm -rf ~/.magick
-rm /usr/include/iconv.h
-rm /usr/include/libcharset.h
-rm /usr/include/localcharset.h
 rm -rf /Library/LaunchDaemons/com.tokaido.firewall.plist
 launchctl remove com.tokaido.firewall
